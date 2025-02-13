2a268938ecc600aaa5ed0e2dfc71f4ac00a134e5
JaredMeeks <jared13meeks56@gmail.com>
2025-02-13 18:01:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,64 @@
+let humanScore = 0;
+let computerScore = 0;
+// let choice = prompt("Choose: Rock, Paper, or Scissors");
+
+
 function getComputerChoice() {
     let randomChoice =  Math.floor(Math.random() * 3);
 
     if (randomChoice === 0) {
-        return "Rock";
+        return "ROCK";
     } else if (randomChoice === 1) {
-        return "Paper";
+        return "PAPER";
     } else if (randomChoice === 2) {
-        return "Scissors";
+        return "SCISSORS";
     }
     return randomChoice;
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function getHumanChoice() {
     let choice = prompt("Choose: Rock, Paper, or Scissors");
-
     if (choice.toUpperCase() === "ROCK") {
-        return "Rock";
+        return "ROCK";
     } else if (choice.toUpperCase() === "PAPER") {
-        return "Paper";
+        return "PAPER";
     } else if (choice.toUpperCase() === "SCISSORS") {
-        return "Scissors";
+        return "SCISSORS";
     } else {
         return "Invalid Choice";
     }
 }
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(humanChoice, computerChoice) {
+    
+    if (humanSelection.toUpperCase() === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (humanSelection.toUpperCase() === "ROCK" && computerSelection === "SCISSORS") ||
+        (humanSelection.toUpperCase() === "PAPER" && computerSelection === "ROCK") ||
+        (humanSelection.toUpperCase() === "SCISSORS" && computerSelection === "PAPER")
+    ) {
+        return "You WIN! " + humanSelection + " beats " + computerSelection + "!";
+        humanScore++;
+    } else {
+        return "You Lose. " + computerSelection + " beats " + humanSelection + ".";
+        computerScore++;
+    }
+}
+console.log(playRound(humanSelection, computerSelection));
+// playRound(humanSelection, computerSelection)
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    getComputerChoice();
+    getHumanChoice();
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+}
